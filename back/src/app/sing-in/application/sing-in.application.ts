e071c8c470c987583d9867929f4599e5477c6c30
jcarloshg <carlosj12336@gmail.com>
2025-12-29 12:09:31 -0600

    add docs for auth

--- back/src/app/sing-in/application/sing-in.application.ts
+++ back/src/app/sing-in/application/sing-in.application.ts
@@ -2,9 +2,7 @@ import { CustomResponse } from "@/app/shared/domain/models/CustomResponse";
 import { SingInUseCase, SingInRequestProps } from "../domain/sing-in.usecase";
 import { GetByEmailUserRepoInMemory } from "../infra/persistence/memory/GetByIdUser.repo.inmemory";
 
-export const singInApplication = async (
-    req: SingInRequestProps
-): Promise<CustomResponse> => {
+export const singInApplication = async (req: SingInRequestProps): Promise<CustomResponse> => {
 
     try {
         const getByEmailUserRepo: GetByEmailUserRepoInMemory = new GetByEmailUserRepoInMemory();
